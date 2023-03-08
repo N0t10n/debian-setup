02ca5908001e63d9361aa42c6089ce4fc374c956
N0t10n <0ts.notion@gmail.com>
2023-03-08 08:21:20 +0100

    installation of icons fixed

--- icons/install_icons.sh
+++ icons/install_icons.sh
@@ -6,6 +6,6 @@ echo "Installing reversal icons theme..."
 tar -xf Reversal.tar.xz
 
 # Instalation
-sudo mv Reversal ~/.local/share/icons
-sudo mv Reversal-dark ~/.local/share/icons
+sudo mv Reversal/ ~/.local/share/icons
+sudo mv Reversal-dark/ ~/.local/share/icons
 echo "Reversal icons installed succesfully"
